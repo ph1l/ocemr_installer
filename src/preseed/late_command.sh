b5b4b2e168dcb573ddd6ea0a846275aecc293c17
Philip J Freeman <philip.freeman@gmail.com>
2012-07-08 01:49:31 -0500

    no need to install masterfiles manually (it's packaged now)

--- src/preseed/late_command.sh
+++ src/preseed/late_command.sh
@@ -7,10 +7,6 @@
 mkdir -p /target/var/packages
 cp -r /hd-media/repo /target/var/packages/
 
-# Copy cfengine policy to target
-mkdir -p /target/var/cfengine
-cp -rv /hd-media/masterfiles /target/var/cfengine/
-
 # Setup first run script
 cp /target/etc/rc.local /target/etc/rc.local.orig
 cp /hd-media/preseed/rc.local.first /target/etc/rc.local
