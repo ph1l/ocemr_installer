791e5edc842643840b3307fa62bcb6e90b97b5fb
Philip J Freeman <elektron@halo.nu>
2014-06-12 02:12:00 -0700

    generate image file w/o device, update shit

--- setup_installer.sh
+++ setup_installer.sh
@@ -4,13 +4,14 @@
 
 #### CONFIG SECTION #####
 
-DEB_REL="6.0.9"		# Debian Release. Currently must be a release of squeeze (6.x).
+ISO_TYPE="netinst"			# type of debian installer ISO (netinst, CD-1, etc..)
+IMG_SIZE=$(( 256 * 1024 * 1024 ))	# for netinst set to 512M for CD-1 set to 1024M
 
-CODENAME="squeeze"	# Debian Release codename. (for downloading INSTALLER media from: http://ftp.debian.org/debian/dists/${CODENAME}/main/installer-${ARCH}/ )
+# following options untested if changed:
 
-ISO_TYPE="CD-1"	# Which type of ISO to include on installer device.
-
-ARCH=i386		# Arch. Currently only tested for i386.
+CODENAME="squeeze"			# release codename
+DEB_REL="6.0.9"				# release version
+ARCH="i386"				# architecture
 
 ## END CONFIG SECTION ###
 
@@ -21,44 +22,47 @@ ISO_FILENAME=debian-${DEB_REL}-${ARCH}-${ISO_TYPE}.iso
 INSTALLER_BASE_URL=http://ftp.debian.org/debian/dists/${CODENAME}/main/installer-${ARCH}/current/images/hd-media/
 
 if [ -z "${1}" ]; then
-	echo "Usage: ${0} <DEVICE>"
+	echo "Usage: ${0} <FILE>"
 	exit 2
 fi
 
-USB_DEV=${1}
+IMAGE_FILE=${1}
 
 if [ ! -e debian-${DEB_REL}-${ARCH}-${ISO_TYPE}.iso ]; then
 	wget ${ISO_URL_BASE}/${ISO_FILENAME}
 fi
 
-parted -s ${USB_DEV} mklabel msdos
-USB_SIZE=`sudo parted -sm ${USB_DEV} unit b print | grep ^${USB_DEV} | cut -d: -f2 | cut -dB -f1`
-parted -s ${USB_DEV} mkpart primary ext2 1048576B $(( ${USB_SIZE} - 1 ))B
-parted -s ${USB_DEV} set 1 boot on
+dd if=/dev/zero of=${IMAGE_FILE} seek=${IMG_SIZE} count=1k bs=1
 
-kpartx ${USB_DEV}
+LOOP_DEV=`sudo losetup -f --show ${IMAGE_FILE}`
 
-mke2fs ${USB_DEV}1
+sudo parted -s ${LOOP_DEV} mklabel msdos
+sudo parted -s ${LOOP_DEV} mkpart primary ext2 1048576B $(( ${IMG_SIZE} - 1024 ))B
+sudo parted -s ${LOOP_DEV} set 1 boot on
+sudo partx -a ${LOOP_DEV} || true
+sudo mke2fs ${LOOP_DEV}p1
 
 mkdir -p /tmp/$$.usb
 
-mount ${USB_DEV}1 /tmp/$$.usb
+sudo mount ${LOOP_DEV}p1 /tmp/$$.usb
 
-grub-install --root-directory=/tmp/$$.usb ${USB_DEV}
-cat src/grub.cfg | sed -e "s/@@ARCH@@/${ARCH}/g" -e "s/@@DEB_REL@@/${DEB_REL}/g"> /tmp/$$.usb/boot/grub/grub.cfg
+sudo grub-install --root-directory=/tmp/$$.usb --modules="ext2 part_msdos" ${LOOP_DEV}
+cat src/grub.cfg | sed -e "s/@@ARCH@@/${ARCH}/g" -e "s/@@DEB_REL@@/${DEB_REL}/g"| sudo tee /tmp/$$.usb/boot/grub/grub.cfg
 
-mkdir -p /tmp/$$.usb/debian-installer-${DEB_REL}-${ARCH}
-wget -O /tmp/$$.usb/debian-installer-${DEB_REL}-${ARCH}/vmlinuz ${INSTALLER_BASE_URL}/vmlinuz
-wget -O /tmp/$$.usb/debian-installer-${DEB_REL}-${ARCH}/initrd.gz ${INSTALLER_BASE_URL}/initrd.gz
+sudo mkdir -p /tmp/$$.usb/debian-installer-${DEB_REL}-${ARCH}
+sudo wget -O /tmp/$$.usb/debian-installer-${DEB_REL}-${ARCH}/vmlinuz ${INSTALLER_BASE_URL}/vmlinuz
+sudo wget -O /tmp/$$.usb/debian-installer-${DEB_REL}-${ARCH}/initrd.gz ${INSTALLER_BASE_URL}/initrd.gz
 
-cp -r src/preseed /tmp/$$.usb/
+sudo cp -r src/preseed /tmp/$$.usb/
 
-mkdir -p /tmp/$$.usb/repo
-cp -v repo/*_all.deb repo/*_${ARCH}.deb /tmp/$$.usb/repo/
-( cd /tmp/$$.usb; dpkg-scanpackages repo/ /dev/null | gzip > repo/Packages.gz )
+sudo mkdir -p /tmp/$$.usb/repo
+sudo cp -v repo/*_all.deb repo/*_${ARCH}.deb /tmp/$$.usb/repo/
+( cd /tmp/$$.usb; dpkg-scanpackages repo/ /dev/null | sudo tee repo/Packages && sudo gzip repo/Packages )
 
-cp -v debian-${DEB_REL}-${ARCH}-${ISO_TYPE}.iso /tmp/$$.usb
+sudo cp -v debian-${DEB_REL}-${ARCH}-${ISO_TYPE}.iso /tmp/$$.usb
 
-umount /tmp/$$.usb
-
+# cleanup
+sudo umount /tmp/$$.usb
+sudo partx -d ${LOOP_DEV}p1
+sudo losetup -d ${LOOP_DEV}
 rmdir /tmp/$$.usb
