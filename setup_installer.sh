dfb3baa67923e63b98fc9cbd601a55b08d742007
Philip J Freeman <elektron@halo.nu>
2014-06-11 21:32:59 -0700

    Document script settings
    These should prolly be command line arguments

--- setup_installer.sh
+++ setup_installer.sh
@@ -2,9 +2,23 @@
 #
 #
 
-DEB_REL="6.0.9"
-CODENAME="squeeze"
-ISO_TYPE="CD-1"
+#### CONFIG SECTION #####
+
+DEB_REL="6.0.9"		# Debian Release. Currently must be a release of squeeze (6.x).
+
+CODENAME="squeeze"	# Debian Release codename. (for downloading INSTALLER media from: http://ftp.debian.org/debian/dists/${CODENAME}/main/installer-${ARCH}/ )
+
+ISO_TYPE="CD-1"	# Which type of ISO to include on installer device.
+
+ARCH=i386		# Arch. Currently only tested for i386.
+
+## END CONFIG SECTION ###
+
+
+
+ISO_URL_BASE=http://cdimage.debian.org/cdimage/archive/${DEB_REL}/${ARCH}/iso-cd
+ISO_FILENAME=debian-${DEB_REL}-${ARCH}-${ISO_TYPE}.iso
+INSTALLER_BASE_URL=http://ftp.debian.org/debian/dists/${CODENAME}/main/installer-${ARCH}/current/images/hd-media/
 
 if [ -z "${1}" ]; then
 	echo "Usage: ${0} <DEVICE>"
@@ -13,10 +27,6 @@ fi
 
 USB_DEV=${1}
 
-ARCH=i386
-
-ISO_URL_BASE=http://cdimage.debian.org/cdimage/archive/${DEB_REL}/${ARCH}/iso-cd
-ISO_FILENAME=debian-${DEB_REL}-${ARCH}-${ISO_TYPE}.iso
 if [ ! -e debian-${DEB_REL}-${ARCH}-${ISO_TYPE}.iso ]; then
 	wget ${ISO_URL_BASE}/${ISO_FILENAME}
 fi
@@ -37,7 +47,6 @@ mount ${USB_DEV}1 /tmp/$$.usb
 grub-install --root-directory=/tmp/$$.usb ${USB_DEV}
 cat src/grub.cfg | sed -e "s/@@ARCH@@/${ARCH}/g" -e "s/@@DEB_REL@@/${DEB_REL}/g"> /tmp/$$.usb/boot/grub/grub.cfg
 
-INSTALLER_BASE_URL=http://ftp.debian.org/debian/dists/${CODENAME}/main/installer-${ARCH}/current/images/hd-media/
 mkdir -p /tmp/$$.usb/debian-installer-${DEB_REL}-${ARCH}
 wget -O /tmp/$$.usb/debian-installer-${DEB_REL}-${ARCH}/vmlinuz ${INSTALLER_BASE_URL}/vmlinuz
 wget -O /tmp/$$.usb/debian-installer-${DEB_REL}-${ARCH}/initrd.gz ${INSTALLER_BASE_URL}/initrd.gz
