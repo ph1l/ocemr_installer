24e2f990013927912107eec5c9d5b00f6a30a00b
Philip J Freeman <elektron@halo.nu>
2014-06-13 23:02:46 -0700

    working in jessie

--- src/preseed/late_command.sh
+++ src/preseed/late_command.sh
@@ -8,5 +8,6 @@ mkdir -p /target/var/packages
 cp -r /hd-media/repo /target/var/packages/
 
 # Setup first run script
-cp /target/etc/rc.local /target/etc/rc.local.orig
-cp /hd-media/preseed/rc.local.first /target/etc/rc.local
+cp /hd-media/preseed/rc.local.first /target/etc/
+mv /target/etc/rc.local /target/etc/rc.local.orig
+cp /hd-media/preseed/rc.local.new /target/etc/rc.local
