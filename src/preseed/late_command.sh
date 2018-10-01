45a2849a04cb8154361f222607abfde45b9e4bd2
Pat Freeman <github@e2r4.com>
2018-10-01 11:16:32 -0500

    security updates for hardware and d-i fix

--- src/preseed/late_command.sh
+++ src/preseed/late_command.sh
@@ -3,7 +3,7 @@
 
 # Fix grub's root device definition for 4.17
 # Reinstalling pulls in more dependencies and probably fixes it
-in-target apt-get purge -y `chroot /target dpkg -l | grep linux-image-4.17 | cut -d" " -f3`
+in-target apt-get purge -y `chroot /target dpkg -l | grep linux-image-4.18 | cut -d" " -f3`
 in-target apt-get install -y linux-image-amd64
 
 # Setup first run script
